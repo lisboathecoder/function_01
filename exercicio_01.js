983d6005ea9f5a96a14e2a4227724b3ee04c71a8
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-05-23 16:22:33 -0300

    commit

--- exercicio_01.js
+++ exercicio_01.js
@@ -1,3 +1,8 @@
-function numPrimo(n){
-    return numPrimo
+function ehPrimo(numero){
+    if (numero % 3 === 1 && numero / numero) {
+        return true
+    } else {
+        return false
+    }
 }
+console.log(ehPrimo(11))
---
+++ exercicio_02.js
@@ -0,0 +1,8 @@
+function inverterArray(arr) {
+    return inverter = 0;
+    for(let i = 0; i < arr.length; i--){
+        inverter = 0;
+    }
+    return inverter;
+}
+console.log(inverterArray[10, 2, 3, 5,23]);
