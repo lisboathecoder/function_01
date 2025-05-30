4e3f91108b776c4b8eea396521045bae607c386c
lisboathecoder <gustavo.lisboa2009@gmail.com>
2025-05-30 14:25:43 -0300

    commit

--- exercicio_05.js
+++ exercicio_05.js
@@ -1,10 +1,10 @@
 function somaImpares(arr) {
-    let impares = 0;
-    for(let i = 0; i < impares.length; i++){
-        if(impares[i] % 2 !== 0){
-            impares += 1;
-        }
+  let impares = 0;
+  for (let i = 0; i < impares.length; i++) {
+    if (impares[i] % 2 !== 0) {
+      impares += 1;
     }
-    console.log(impares)
+  }
+  console.log(impares);
 }
-console.log(somaImpares[20, 30, 15, 39, 17, 25, 9])
+console.log(somaImpares[(20, 30, 15, 39, 17, 25, 9)]);
